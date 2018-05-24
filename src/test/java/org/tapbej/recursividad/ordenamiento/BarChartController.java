42163bdcec74de55bf35055d01a3b71f1abe8586
fernando.pinedo@outlook.com <fernando.pinedo@outlook.com>
2018-05-24 18:47:26 -0500

    Agrega prueba para trabajar con graficas

---
+++ src/test/java/org/tapbej/recursividad/ordenamiento/BarChartController.java
@@ -0,0 +1,13 @@
+package org.tapbej.recursividad.ordenamiento;
+
+import javafx.fxml.FXML;
+import javafx.scene.chart.BarChart;
+
+public class BarChartController
+{
+	@FXML
+	private BarChart BarChart1;
+
+
+
+}
---
+++ src/test/java/org/tapbej/recursividad/ordenamiento/TestMain.java
@@ -0,0 +1,44 @@
+package org.tapbej.recursividad.ordenamiento;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import org.tapbej.recursividad.Main;
+
+import java.io.IOException;
+
+public class TestMain extends Application
+{
+	private Stage primaryStage;
+	public void showBarChartsScreen()
+	{
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(Main.class.getResource("vista/BarChartTest.fxml"));
+		AnchorPane ventana = null;
+		try
+		{
+			ventana =  (AnchorPane) loader.load();
+		} catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		primaryStage = new Stage();
+		primaryStage.setScene(new Scene(ventana));
+		primaryStage.show();
+//		MenuControlador controlador = loader.getController();
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception
+	{
+		this.primaryStage = primaryStage;
+		showBarChartsScreen();
+	}
+
+	public static void main(String [] args)
+	{
+		launch(args);
+	}
+}
