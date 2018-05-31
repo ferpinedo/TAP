4e3f828094658e1ffdd58380e8f3abfca56af372
fernando.pinedo@outlook.com <fernando.pinedo@outlook.com>
2018-05-31 17:29:47 -0500

    Crea clase movement

---
+++ src/main/java/org/tapbej/proyectofinal/modelo/Movement.java
@@ -0,0 +1,14 @@
+package org.tapbej.proyectofinal.modelo;
+
+public class Movement
+{
+	public int from;
+	public int to;
+
+	public Movement(int from, int to)
+	{
+		this.from = from;
+		this.to = to;
+	}
+
+}
