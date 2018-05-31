761af77a5842897f3f18069478dd8d5502763a4c
fernando.pinedo@outlook.com <fernando.pinedo@outlook.com>
2018-05-31 02:47:02 -0500

    Enum para clasificar los metodos de ordenamiento

---
+++ src/main/java/org/tapbej/proyectofinal/modelo/SortMethod.java
@@ -0,0 +1,13 @@
+package org.tapbej.proyectofinal.modelo;
+
+public enum SortMethod
+{
+	SELECTION_SORT,
+	BUBBLE_SORT,
+	INSERTION_SORT,
+	SHAKER_SORT,
+	QUICK_SORT,
+	MERGE_SORT,
+	HEAPS_SORT,
+	RADIX_SORT
+}
