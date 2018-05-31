1d2a3ae1ac17d2c79202640998626e85f2fbcc8e
fernando.pinedo@outlook.com <fernando.pinedo@outlook.com>
2018-05-31 10:12:50 -0500

    Hace prueba con 4 metodos de ordenamiento

--- src/test/java/org/tapbej/proyectofinal/ordenamiento/BarChartController.java
+++ src/test/java/org/tapbej/proyectofinal/ordenamiento/BarChartController.java
@@ -1,5 +1,6 @@
 package org.tapbej.proyectofinal.ordenamiento;
 
+import com.sun.prism.PixelFormat;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
@@ -7,10 +8,7 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.*;
 import javafx.scene.layout.GridPane;
 import javafx.util.Duration;
-import org.tapbej.proyectofinal.modelo.GeneradorDatos;
-import org.tapbej.proyectofinal.modelo.OptimizedSortingChart;
-import org.tapbej.proyectofinal.modelo.SortMethod;
-import org.tapbej.proyectofinal.modelo.SortingChart;
+import org.tapbej.proyectofinal.modelo.*;
 import org.tapbej.proyectofinal.util.Sorter;
 
 import java.util.*;
@@ -53,13 +51,25 @@ public class BarChartController
 	@FXML
 	public void initialize()
 	{
+		int size = 50;
+		int interval = 100;
+		int[] data = GeneradorDatos.casoPromedio(size);
 
-//		SortingChart sortingChart = new SortingChart(GeneradorDatos.peorCaso(30), SortMethod.SELECTION_SORT);
-//		gridPane.add(sortingChart, 1, 2);
-//		sortingChart.sort(200);
-		SortingChart sortingChart2 = new SortingChart(GeneradorDatos.casoPromedio(100), SortMethod.SELECTION_SORT);
-		gridPane.add(sortingChart2, 2, 2);
-		sortingChart2.sort(100);
+		SortingChart sortingChart = new SortingChart(data, SortMethod.SELECTION_SORT);
+		gridPane.add(sortingChart, 0, 0);
+		sortingChart.sort(interval);
+
+		SortingChart sortingChart2 = new SortingChart(data, SortMethod.BUBBLE_SORT);
+		gridPane.add(sortingChart2, 1, 0);
+		sortingChart2.sort(interval);
+
+		SortingChart sortingChart3 = new SortingChart(data, SortMethod.INSERTION_SORT);
+		gridPane.add(sortingChart3, 0, 1);
+		sortingChart3.sort(interval);
+
+		SortingChart sortingChart4 = new SortingChart(data, SortMethod.SHAKER_SORT);
+		gridPane.add(sortingChart4, 1, 1);
+		sortingChart4.sort(interval);
 
 	}
 
